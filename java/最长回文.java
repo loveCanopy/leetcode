de118020741d70d2b38dc2f42bc1b13e93f0e12d
lovecanopy <1039431583@qq.com>
2017-03-01 19:44:00 +0800

    最长回文

---
+++ java/最长回文.java
@@ -0,0 +1,16 @@
+#声明一个set set若为空，则没有单一项数据  count保留字符串中重复字符的个数 
+public int longestPalindrome(String s) {
+        if(s==null || s.length()==0) return 0;
+        HashSet<Character> hs = new HashSet<Character>();
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            if(hs.contains(s.charAt(i))){
+                hs.remove(s.charAt(i));
+                count++;
+            }else{
+                hs.add(s.charAt(i));
+            }
+        }
+        if(!hs.isEmpty()) return count*2+1;
+        return count*2;
+}
