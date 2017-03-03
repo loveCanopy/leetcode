b363b02795629b95d137477280441a5fb00971d4
lovecanopy <1039431583@qq.com>
2017-03-03 16:17:04 +0800

    stack

---
+++ java/next_greate.java
@@ -0,0 +1,18 @@
+public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num)
+                map.put(stack.pop(), num);
+            stack.push(num);
+        }   
+        for (int i = 0; i < findNums.length; i++)
+            findNums[i] = map.getOrDefault(findNums[i], -1);
+        return findNums;
+    }
+
+
+
+stack 弹出当前栈中的最大值
+map nums中比1 大的数是3 比3大的数是4
+例如1,3,4,2  保留（1,3） （3,4）
