96b3cd455413199b486040ae55a63ef536b84c2e
root <root@DESKTOP-T7K5GSI.localdomain>
2017-08-17 11:15:30 +0800

    fix bug

--- 企业编程题/吉比特.java
+++ 企业编程题/吉比特.java
@@ -1,29 +1,37 @@
 /*
  *输入M、N，1 < M < N < 1000000，求区间[M,N]内的所有素数的个数。素数定义：除了1以外，只能被1和自己整除的自然数称为素数
  * */
-public static int getSu1(int m,int n){
-		int count=0;
-		for(int i=m;i<=n;i++){
-			int j=2;
-			if(i!=1){
-				if(i==2){
-					count++;
-				}
-			while(i%j!=0){
-				if(j==i-1){
-					System.out.println(i);
-					count++;
-					break;
-				}else{
-					j++;
-				}
-			}
-			}
-			
-		}
-		return count;
-		
+链接：https://www.nowcoder.com/questionTerminal/0fb0b1d623cb4f3c86b53df0817246b9
+来源：牛客网
+
+public class prime {
+	public static void main(String[] args) {
+	判断区间内有几个素数
+	countPrime()；
 	}
+	private static void countPrime(){
+	Scanner sc = new Scanner(System.in);
+	int start = sc.nextInt();
+	int end = sc.nextInt(); 
+	int count = 0;
+	for (int i = start; i <= end; i++) {
+		if (i != 1) {
+	int flag = 0;
+	for (int j = 2; j < i; j++) {
+		if (i % j == 0) {
+		flag = 1;
+		break;
+	}
+	}
+	if (flag == 0){
+	count++;
+	} 
+	} 
+	System.out.println(count);
+}
+}
+
+
 /*
 给定一个未排序的数列，找到此数列在已排序状态下的两个相邻值的最大差值，少于两个值时返回0。例如：给定数列 [1,3,2,0,1,6,8] 则 最大差值为3。注意：请尽量使用时间复杂度为O(n)的方案。
 */
